437e327242dd92ec6791b4634f408f09b9289814
Dustin Goodwin <dustingoodwin15@gmail.com>
2025-07-27 15:59:52 -0500

    about changes

---
+++ src/BandMemberCard/BandMemberCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./BandMemberCard.css";
+
+function BandMemberCard({ image, name, role }) {
+  return (
+    <div className="band-member-card">
+      <div className="card-image"></div>
+    </div>
+  );
+}
